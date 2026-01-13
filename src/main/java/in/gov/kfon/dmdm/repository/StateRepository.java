91d96e01aa62aff675e18eda4931c5142d094a61
arun-xaviour <arunxaviour@kfonbss.co.in>
2026-01-13 14:46:08 +0530

    Implemented the api for the state by code

--- src/main/java/in/gov/kfon/dmdm/repository/StateRepository.java
+++ src/main/java/in/gov/kfon/dmdm/repository/StateRepository.java
@@ -1,7 +1,13 @@
 package in.gov.kfon.dmdm.repository;
 
 import in.gov.kfon.dmdm.model.State;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StateRepository extends JpaRepository<State, UUID> {}
+@Repository
+public interface StateRepository extends JpaRepository<State, UUID> {
+    List<State> findByCodeAndIsActive(String code, boolean active);
+}
