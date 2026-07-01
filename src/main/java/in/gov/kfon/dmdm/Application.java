851c2d8d148aad967662161f74bd338d7aad1739
arun-xaviour <arunxaviour@kfonbss.co.in>
2026-07-01 11:44:06 +0530

    implemented the change

--- src/main/java/in/gov/kfon/dmdm/Application.java
+++ src/main/java/in/gov/kfon/dmdm/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "in.gov.kfon")
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 @ComponentScan({"in.gov.kfon"})
 @EnableCaching
