d5f2cc622b72384cb7b389cc2895d8a4854d5848
arun-xaviour <arunxaviour@kfonbss.co.in>
2026-07-01 09:39:17 +0530

    implemented the changes in the revenue share settings

--- src/main/java/in/gov/kfon/dmdm/repository/ServiceTypeRepository.java
+++ src/main/java/in/gov/kfon/dmdm/repository/ServiceTypeRepository.java
@@ -4,7 +4,4 @@ import in.gov.kfon.dmdm.model.ServiceType;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServiceTypeRepository extends JpaRepository<ServiceType, UUID> {
-
-  java.util.Optional<ServiceType> findByTypeId(Integer typeId);
-}
+public interface ServiceTypeRepository extends JpaRepository<ServiceType, UUID> {}
