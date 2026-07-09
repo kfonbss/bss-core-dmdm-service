13b47821fb7050f3605ef3e1cdd6f9b8fedb8a80
Unnimayakrishnan <unnimayakrishnan@Unnimayakrishnans-MacBook-Pro.local>
2026-07-09 18:05:41 +0530

    New domain whitlist

--- src/main/java/in/gov/kfon/dmdm/Config/WebConfig.java
+++ src/main/java/in/gov/kfon/dmdm/Config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig {
                 "https://dev.kfon.co.in",
                 "https://qa.kfon.co.in",
                 "https://*.sritindia.com",
-                "https://bssdemo.sritindia.com")
+                "https://bssdemo.sritindia.com","https://rwbssdev.sritindia.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
             .allowedHeaders("*")
             .allowCredentials(true);
