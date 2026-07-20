768a8fbee5446be62e837dd2f0d7038799c0c233
Ayisha Sabu <ayisha@kfonbss.co.in>
2026-07-20 18:05:23 +0530

    territory list api

--- src/main/java/in/gov/kfon/dmdm/repository/RailTerritoryNameRepository.java
+++ src/main/java/in/gov/kfon/dmdm/repository/RailTerritoryNameRepository.java
@@ -4,9 +4,13 @@ import in.gov.kfon.dmdm.model.RailTerritoryName;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RailTerritoryNameRepository extends JpaRepository<RailTerritoryName, UUID> {
-  List<RailTerritoryName> findByStcodeAndStatus(Integer stcode, Integer status);
+  @Query("select r from RailTerritoryName r where r.cCode = :cCode and r.status = :status")
+  List<RailTerritoryName> findByCCodeAndStatus(
+      @Param("cCode") String cCode, @Param("status") Integer status);
 }
