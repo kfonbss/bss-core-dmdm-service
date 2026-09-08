4276cc1d8acd3ff528e69a251f1513d76c86c6d8
ayisha-srit <ayishasabu.hakkim@sritindia.com>
2026-09-08 10:44:30 +0530

    bank details - tenant id based

--- src/main/java/in/gov/kfon/dmdm/repository/BankDetailsRepository.java
+++ src/main/java/in/gov/kfon/dmdm/repository/BankDetailsRepository.java
@@ -1,10 +1,13 @@
 package in.gov.kfon.dmdm.repository;
 
 import in.gov.kfon.dmdm.model.BankDetails;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BankDetailsRepository extends JpaRepository<BankDetails, UUID> {
   Optional<Object> findByBankIfscCode(String ifsc);
+
+  List<BankDetails> findByBankStateIgnoreCase(String bankState);
 }
