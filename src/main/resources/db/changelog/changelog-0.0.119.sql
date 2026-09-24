19cba8507e81be389e7992a6a7f3a39b3c146c01
neenu-srit <neenu.rp@sritindia.com>
2026-09-24 09:53:42 +0530

    gateway type added

--- src/main/resources/db/changelog/changelog-0.0.119.sql
+++ src/main/resources/db/changelog/changelog-0.0.119.sql
@@ -1,5 +1,6 @@
+
 INSERT INTO gateway (id, code, type, gateid)
-SELECT next_ids.id, 'G05', 'Paytm', next_ids.gateid
+SELECT next_ids.id, 'PAYTM', 'Paytm', next_ids.gateid
 FROM (
   SELECT COALESCE(MAX(id), 0) + 1 AS id, COALESCE(MAX(gateid), 0) + 1 AS gateid FROM gateway
 ) next_ids
